c42f7f90abbb30925eef991a349b5acd97085ca0
Mat Kelly <me@matkelly.com>
2017-11-15 15:08:21 -0500

    CRLF to space for faster and more fluid vocals.

--- sing.sh
+++ sing.sh
@@ -6,6 +6,9 @@ pdf=$1
 convert $pdf -append image-version.png
 tesseract image-version.png text-version -psm 11
 
+# Replace two consecutive line breaks with a space
+sed -i.bak -n -e 'H;${x;s/\n/ /g;p;}' text-version.txt
+
 say -o audio-version.aiff < text-version.txt
 rm image-version.png
 rm text-version.txt
