8bf4e141cc3655457f7da321b61efafcb0616282
Mat Kelly <me@matkelly.com>
2017-11-15 20:39:13 -0500

    Organ when you can.

--- sing.sh
+++ sing.sh
@@ -9,6 +9,13 @@ tesseract image-version.png text-version -psm 11
 # Replace two consecutive line breaks with a space
 sed -i.bak -n -e 'H;${x;s/\n/ /g;p;}' text-version.txt
 
-say -o audio-version.aiff < text-version.txt
+# Make it more musical...if you can
+VOICE="Alex"
+isItOrganTime=$(say -v ? | grep "Pipe Organ" | wc -l)
+if (($isItOrganTime > 0)); then
+  VOICE="Pipe Organ"
+fi
+
+say -v "$VOICE" -o audio-version.aiff < text-version.txt
 rm image-version.png
 rm text-version.txt
